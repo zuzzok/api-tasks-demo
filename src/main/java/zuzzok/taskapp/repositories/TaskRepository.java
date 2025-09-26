1f508a92a57a32d6a929ecf66e1f39a6201ee972
Gino Canestrari <canestrari@protonmail.com>
2025-09-26 05:56:33 -0300

    ✨ feat(repository): update findByTitle method to return Optional<Task> in TaskRepository

--- src/main/java/zuzzok/taskapp/repositories/TaskRepository.java
+++ src/main/java/zuzzok/taskapp/repositories/TaskRepository.java
@@ -1,11 +1,15 @@
 package zuzzok.taskapp.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import zuzzok.taskapp.models.Task;
 
+@Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
   
-  public Task findByTitle(String title);
+  public Optional<Task> findByTitle(String title);
 
 }
