553f92822130c07dcbaa3ad02cb4f2ec4d9b4fc9
Gino Canestrari <canestrari@protonmail.com>
2025-09-28 15:04:56 -0300

    ✨ feat(service): enhance TaskService with exception handling and updateTask method

--- src/main/java/zuzzok/taskapp/services/TaskService.java
+++ src/main/java/zuzzok/taskapp/services/TaskService.java
@@ -1,13 +1,16 @@
 package zuzzok.taskapp.services;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import zuzzok.taskapp.models.Task;
 import zuzzok.taskapp.models.TaskStatus;
 import zuzzok.taskapp.payloads.TaskRequest;
+import zuzzok.taskapp.payloads.TaskUpdateRequest;
 import zuzzok.taskapp.repositories.TaskRepository;
 
 @Service
@@ -21,17 +24,36 @@ public class TaskService {
   }
 
   public Task getTaskById(Long id) {
-    return taskRepository.findById(id).orElse(null);
+    return taskRepository.findById(id).orElseThrow(
+      () -> new RuntimeException("Task not found")
+    ); // TODO: Reemplazar con excepción personalizada
   }
 
+  @Transactional
   public Task createTask(TaskRequest request) {
     var task = Task.builder()
       .title(request.getTitle())
       .description(request.getDescription())
       .status(TaskStatus.PENDING)
       .build();
-      
+
     return taskRepository.save(task);
+  }
+
+  @Transactional
+  public Task updateTask(Long id, TaskUpdateRequest request) {
+    
+    var task = taskRepository.findById(id).orElseThrow(
+      () -> new RuntimeException("Task not found")
+    ); // TODO: Reemplazar con excepción personalizada
+
+    task.setTitle(request.getTitle());
+    task.setDescription(request.getDescription());
+    task.setStatus(request.getStatus() != null ? request.getStatus() : task.getStatus());
+    task.setUpdatedAt(LocalDateTime.now());
+    
+    return task;
+
   }
 
 }
