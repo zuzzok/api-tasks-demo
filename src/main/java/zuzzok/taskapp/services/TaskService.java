b98ae1df7d99de4b6e30acfa648ee44f96e24395
Gino Canestrari <canestrari@protonmail.com>
2025-09-28 10:39:49 -0300

    ✨ feat(service): implement createTask method to save a new task

--- src/main/java/zuzzok/taskapp/services/TaskService.java
+++ src/main/java/zuzzok/taskapp/services/TaskService.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 import zuzzok.taskapp.models.Task;
+import zuzzok.taskapp.models.TaskStatus;
+import zuzzok.taskapp.payloads.TaskRequest;
 import zuzzok.taskapp.repositories.TaskRepository;
 
 @Service
@@ -20,6 +22,16 @@ public class TaskService {
 
   public Task getTaskById(Long id) {
     return taskRepository.findById(id).orElse(null);
+  }
+
+  public Task createTask(TaskRequest request) {
+    var task = Task.builder()
+      .title(request.getTitle())
+      .description(request.getDescription())
+      .status(TaskStatus.PENDING)
+      .build();
+      
+    return taskRepository.save(task);
   }
 
 }
