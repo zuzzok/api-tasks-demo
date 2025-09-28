4bc78c44fb307781f27fdbd6c656b5d28640f89b
Gino Canestrari <canestrari@protonmail.com>
2025-09-28 15:30:37 -0300

    ✨ feat(controller): add deleteTask endpoint to remove a task

--- src/main/java/zuzzok/taskapp/controllers/TaskController.java
+++ src/main/java/zuzzok/taskapp/controllers/TaskController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -56,6 +57,12 @@ public class TaskController {
   public ResponseEntity<TaskResponse> updateTask(@PathVariable Long id, @RequestBody TaskUpdateRequest request) {
     var task = taskService.updateTask(id, request);
     return ResponseEntity.status(HttpStatus.OK).body(TaskResponse.fromEntity(task));
+  }
+
+  @DeleteMapping("/tasks/{id}")
+  public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+    taskService.deleteTask(id);
+    return ResponseEntity.noContent().build();
   }
 
 }
