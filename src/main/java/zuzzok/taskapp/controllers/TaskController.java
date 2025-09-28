60788df797bb2420b24f04b8915214dd19d10478
Gino Canestrari <canestrari@protonmail.com>
2025-09-28 10:39:58 -0300

    ✨ feat(controller): add createTask endpoint to handle task creation

--- src/main/java/zuzzok/taskapp/controllers/TaskController.java
+++ src/main/java/zuzzok/taskapp/controllers/TaskController.java
@@ -2,15 +2,22 @@ package zuzzok.taskapp.controllers;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import zuzzok.taskapp.payloads.TaskRequest;
 import zuzzok.taskapp.payloads.TaskResponse;
 import zuzzok.taskapp.services.TaskService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 
@@ -39,5 +46,12 @@ public class TaskController {
     }
     return ResponseEntity.ok(TaskResponse.fromEntity(task));
   }
+
+  @PostMapping("/tasks")
+  public ResponseEntity<TaskResponse> createTask(@RequestBody @Valid TaskRequest request) {
+    var task = taskService.createTask(request);
+    return ResponseEntity.status(HttpStatus.CREATED).body(TaskResponse.fromEntity(task));
+  }
+  
 
 }
